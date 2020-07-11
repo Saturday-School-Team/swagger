39cb3ce977871aba59de96e2070932b2a8bf7426
Sagar Ssss <sagarbdlove@hotmail.com>
2020-07-11 13:42:51 +0600

    Swagger youtube documentation (Tutorial for Swagger API Documentation in .Net Core 3.1)

---
+++ SchoolApi/Utility/RemoveVersionFromParameter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger.Utility
+{
+    public class RemoveVersionFromParameter : IOperationFilter
+    {
+        // This call remove version from parameter, without it we will have version as parameter 
+        // for all endpoints in swagger UI
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var versionParameter = operation.Parameters.Single(p => p.Name == "version");
+            operation.Parameters.Remove(versionParameter);
+        }
+    }
+}
---
+++ SchoolApi/Utility/ReplaceVersionWithExactValueInPath.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger.Utility
+{
+    // This make replacement of v{version:apiVersion} to real version of corresponding swagger doc.
+    public class ReplaceVersionWithExactValueInPath : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            if (swaggerDoc == null)
+                throw new ArgumentNullException(nameof(swaggerDoc));
+
+            var replacements = new OpenApiPaths();
+            foreach (var (key, value) in swaggerDoc.Paths)
+            {
+                replacements.Add(key.Replace("v{version}", swaggerDoc.Info.Version, StringComparison.InvariantCulture), value);
+            }
+            swaggerDoc.Paths = replacements;
+        }
+    }
+}
+
+
+//public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+//{
+//    swaggerDoc.Paths = (OpenApiPaths)swaggerDoc.Paths
+//        .ToDictionary(
+//            path => path.Key.Replace("v{version}", swaggerDoc.Info.Version),
+//            path => path.Value
+//        );
+
+//    //swaggerDoc.Paths = (OpenApiPaths)lol;
+//}
